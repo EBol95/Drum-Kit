eceba337b5803d4c9cfbd1ddf72fa9c0c419b976
Nontas <tsakoto90@gmail.com>
2022-03-01 13:42:18 +0200

    2nd commit

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0; i<numberOfDrumButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
-    alert("I got clicked")
+    this.style.color = "white";
   });
 }
+
+
+
+
+
+
+
+
+
+
+//
+// var audio = new Audio('sounds/tom-2.mp3');
+// audio.play();
